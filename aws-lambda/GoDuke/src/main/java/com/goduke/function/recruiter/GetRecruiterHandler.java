823dfc214c129b121a4a924935119997ffe6dce1
Sorcuss <31709862+Sorcuss@users.noreply.github.com>
2019-11-30 00:39:57 +0100

    Revert "Revert "recruiter functions refactor""
    This reverts commit 4b59a54182e8322bfd7eb2ae409433a98c936bde.

--- aws-lambda/GoDuke/src/main/java/com/goduke/function/recruiter/GetRecruiterHandler.java
+++ aws-lambda/GoDuke/src/main/java/com/goduke/function/recruiter/GetRecruiterHandler.java
@@ -4,16 +4,25 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.goduke.model.Recruiter;
+import com.google.gson.Gson;
+import com.google.gson.internal.$Gson$Preconditions;
 
-public class GetRecruiterHandler implements RequestHandler<Recruiter, Recruiter> {
+public class GetRecruiterHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
     private DynamoDBMapper dynamoDBMapper = new DynamoDBMapper(AmazonDynamoDBClientBuilder.defaultClient());
     @Override
-    public Recruiter handleRequest(Recruiter recruiter, Context context){
-        Recruiter recruiterToGet = dynamoDBMapper.load(Recruiter.class, recruiter.getId());
-        if(recruiterToGet == null){
-            throw new RuntimeException("Error!: object does not exist.");
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context){
+        String id = input.getPathParameters().get("id");
+        if(id == null){
+            return new APIGatewayProxyResponseEvent().withStatusCode(400).withBody("null id");
         }
-        return recruiterToGet;
+        Recruiter recruiterToGet = dynamoDBMapper.load(Recruiter.class, id);
+        if(recruiterToGet == null){
+            return new APIGatewayProxyResponseEvent().withStatusCode(400).withBody("recruiter does not exist");
+        }
+        Gson gson = new Gson();
+        return new APIGatewayProxyResponseEvent().withStatusCode(200).withBody(gson.toJson(recruiterToGet));
     }
 }
