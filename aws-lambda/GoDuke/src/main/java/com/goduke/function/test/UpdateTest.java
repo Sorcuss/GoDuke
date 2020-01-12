7448c205c8a0aa82551576d7181adb58b8ab958f
Sorcuss <31709862+Sorcuss@users.noreply.github.com>
2020-01-12 01:28:06 +0100

    add translate question in test update

--- aws-lambda/GoDuke/src/main/java/com/goduke/function/test/UpdateTest.java
+++ aws-lambda/GoDuke/src/main/java/com/goduke/function/test/UpdateTest.java
@@ -4,6 +4,7 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.goduke.function.translate.QuestionTranslator;
 import com.goduke.model.Test;
 import com.goduke.validator.TestValidator;
 
@@ -18,6 +19,10 @@ public class UpdateTest implements RequestHandler<Test, Test> {
         Test testToUpdate = dynamoDBMapper.load(Test.class, input.getId());
         if(testToUpdate == null){
             throw new RuntimeException("test does not exist");
+        }
+        if(input.getLanguages().size() == 2){
+            QuestionTranslator questionTranslator = new QuestionTranslator(input);
+            questionTranslator.translate();
         }
         dynamoDBMapper.save(input);
         return input;
