7bf1932f3e85d0cf740d1cdcb1f00be85707ebf2
ZygmuntJakub <216938@edu.p.lodz.pl>
2020-01-18 04:37:42 +0100

    add: custom login page

---
+++ react/test-admin/src/LoginPage.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Login, LoginForm } from 'react-admin';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = ({
+    main: {
+        background: 'linear-gradient(45deg, #3f51b5 30%, #673ab7 90%)',
+        border: 0,
+        borderRadius: 3,
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+        color: 'white',
+        height: 48,
+        padding: '0 30px',
+    },
+    avatar: {
+        background: 'url(https://goduke-react-app.s3.amazonaws.com/logo512.png)',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'contain',
+        height: 80,
+        "background-position": "center"
+    },
+    icon: { display: 'none' },
+});
+
+const CustomLoginForm = withStyles({
+    button: { background: '#F15922' },
+})(LoginForm);
+
+const CustomLoginPage = props => (
+    <Login
+        backgroundImage=""
+        loginForm={<CustomLoginForm />}
+        {...props}
+    />
+);
+
+export default withStyles(styles)(CustomLoginPage);
