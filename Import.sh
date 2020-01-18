df6dc7e92577f56f25ea1c9d249b44c7553f17e6
ZygmuntJakub <216938@edu.p.lodz.pl>
2020-01-18 20:08:05 +0100

    fix: cleaning

--- Import.sh
+++
@@ -1,4 +0,0 @@
-name="bsm"
-for i in {1..5}; do
-    aws cognito-idp admin-create-user --user-pool-id us-east-1_JCWokP4yj --username ${name}0$i@${name}.pl --temporary-password Password0$i! --user-attributes Name="email",Value="${name}0$i@${name}.pl" Name="name",Value="${name}0$i@${name}.pl";
-done
